43a2c39e79cb7f7724d7f7fdc742c256a482a032
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-06-29 13:30:59 +0200

    Fixed same discriminator exception

--- src/main/java/com/buchmais/sarf/node/RuleBasedCriterionDescriptor.java
+++ src/main/java/com/buchmais/sarf/node/RuleBasedCriterionDescriptor.java
@@ -1,5 +1,6 @@
 package com.buchmais.sarf.node;
 
+import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
 
 import java.util.Set;
@@ -7,6 +8,7 @@ import java.util.Set;
 /**
  * @author Stephan Pirnbaum
  */
+@Label("RuleBasedClassificationCriterion")
 public interface RuleBasedCriterionDescriptor<T extends RuleDescriptor> extends ClassificationCriterionDescriptor {
     @Relation("USES")
     @Relation.Outgoing
