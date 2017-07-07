6597edcd20963ddcedd1bcf08986975538755a0a
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-07-07 10:28:50 +0200

    Fixed typing

--- src/main/java/com/buchmais/sarf/classification/criterion/typenaming/TypeNamingCriterionDescriptor.java
+++ src/main/java/com/buchmais/sarf/classification/criterion/typenaming/TypeNamingCriterionDescriptor.java
@@ -1,11 +1,10 @@
 package com.buchmais.sarf.classification.criterion.typenaming;
 
 import com.buchmais.sarf.classification.criterion.data.node.RuleBasedCriterionDescriptor;
-import com.buchmais.sarf.node.PatternDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 /**
  * @author Stephan Pirnbaum
  */
 @Label("TypeNamingCriterion")
-public interface TypeNamingCriterionDescriptor extends RuleBasedCriterionDescriptor<PatternDescriptor> {}
+public interface TypeNamingCriterionDescriptor extends RuleBasedCriterionDescriptor<TypeNamingRuleDescriptor> {}
