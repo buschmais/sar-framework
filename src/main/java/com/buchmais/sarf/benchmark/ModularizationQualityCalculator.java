73c42fd18f15fe0d1ce27ed62337df758cb25b05
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-08-07 13:22:06 +0200

    Made MQ independent of database

--- src/main/java/com/buchmais/sarf/benchmark/ModularizationQualityCalculator.java
+++ src/main/java/com/buchmais/sarf/benchmark/ModularizationQualityCalculator.java
@@ -1,7 +1,6 @@
 package com.buchmais.sarf.benchmark;
 
-import com.buchmais.sarf.SARFRunner;
-import com.buchmais.sarf.repository.MetricRepository;
+import com.buchmais.sarf.classification.criterion.cohesion.Problem;
 
 import java.util.Map;
 import java.util.Objects;
@@ -15,16 +14,15 @@ public class ModularizationQualityCalculator {
     public static Double computeSimilarityBasedMQ(Map<Long, Set<Long>> decomposition) {
         Double intraConnectivity = 0d;
         Double interConnectivity = 0d;
-        MetricRepository metricRepository = SARFRunner.xoManager.getRepository(MetricRepository.class);
         for (Map.Entry<Long, Set<Long>> component1 : decomposition.entrySet()) {
             long[] ids1 = component1.getValue().stream().mapToLong(l -> l).toArray();
             int denominator = ids1.length == 1 ? 1 : ((ids1.length * (ids1.length - 1)) / 2);
-            intraConnectivity += metricRepository.computeSimilarityCohesionInComponent(ids1) / denominator;
+            intraConnectivity += Problem.getInstance().computeSimilarityCohesionInComponent(component1.getValue()) / denominator;
             for (Map.Entry<Long, Set<Long>> component2 : decomposition.entrySet()) {
                 if (!Objects.equals(component1.getKey(), component2.getKey())) {
                     long[] ids2 = component2.getValue().stream().mapToLong(l -> l).toArray();
                     denominator = ((ids1.length + ids2.length) * (ids1.length + ids2.length - 1)) / 2;
-                    interConnectivity += metricRepository.computeSimilarityCouplingBetweenComponents(ids1, ids2) / denominator;
+                    interConnectivity += Problem.getInstance().computeSimilarityCouplingBetweenComponents(component1.getValue(), component2.getValue()) / denominator;
                 }
             }
         }
@@ -36,16 +34,15 @@ public class ModularizationQualityCalculator {
     public static Double computeCouplingBasedMQ(Map<Long, Set<Long>> decomposition) {
         Double intraConnectivity = 0d;
         Double interConnectivity = 0d;
-        MetricRepository metricRepository = SARFRunner.xoManager.getRepository(MetricRepository.class);
         for (Map.Entry<Long, Set<Long>> component1 : decomposition.entrySet()) {
             long[] ids1 = component1.getValue().stream().mapToLong(l -> l).toArray();
             int denominator = ids1.length == 1 ? 1 : ((ids1.length * (ids1.length - 1)));
-            intraConnectivity += metricRepository.computeCouplingCohesionInComponent(ids1) / denominator;
+            intraConnectivity += Problem.getInstance().computeCouplingCohesionInComponent(component1.getValue()) / denominator;
             for (Map.Entry<Long, Set<Long>> component2 : decomposition.entrySet()) {
                 if (!Objects.equals(component1.getKey(), component2.getKey())) {
                     long[] ids2 = component2.getValue().stream().mapToLong(l -> l).toArray();
                     denominator = ((ids1.length + ids2.length) * (ids1.length + ids2.length - 1)) / 2;
-                    interConnectivity += metricRepository.computeCouplingBetweenComponents(ids1, ids2) / denominator;
+                    interConnectivity += Problem.getInstance().computeCouplingBetweenComponents(component1.getValue(), component2.getValue()) / denominator;
                 }
             }
         }
