1beb1027afe4ab7e49e27b67732e90dc821212d1
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-08-07 15:34:22 +0200

    Made MQ independent of optimization objective

--- src/main/java/com/buchmais/sarf/benchmark/ModularizationQualityCalculator.java
+++ src/main/java/com/buchmais/sarf/benchmark/ModularizationQualityCalculator.java
@@ -1,6 +1,7 @@
 package com.buchmais.sarf.benchmark;
 
-import com.buchmais.sarf.classification.criterion.cohesion.Problem;
+import com.buchmais.sarf.classification.criterion.cohesion.evolution.Problem;
+import com.buchmais.sarf.classification.criterion.cohesion.evolution.similarity.SimilarityProblem;
 
 import java.util.Map;
 import java.util.Objects;
@@ -11,33 +12,13 @@ import java.util.Set;
  */
 public class ModularizationQualityCalculator {
 
-    public static Double computeSimilarityBasedMQ(Map<Long, Set<Long>> decomposition) {
+    public static Double computeMQ(Map<Long, Set<Long>> decomposition) {
         Double intraConnectivity = 0d;
         Double interConnectivity = 0d;
         for (Map.Entry<Long, Set<Long>> component1 : decomposition.entrySet()) {
             long[] ids1 = component1.getValue().stream().mapToLong(l -> l).toArray();
             int denominator = ids1.length == 1 ? 1 : ((ids1.length * (ids1.length - 1)) / 2);
-            intraConnectivity += Problem.getInstance().computeSimilarityCohesionInComponent(component1.getValue()) / denominator;
-            for (Map.Entry<Long, Set<Long>> component2 : decomposition.entrySet()) {
-                if (!Objects.equals(component1.getKey(), component2.getKey())) {
-                    long[] ids2 = component2.getValue().stream().mapToLong(l -> l).toArray();
-                    denominator = ((ids1.length + ids2.length) * (ids1.length + ids2.length - 1)) / 2;
-                    interConnectivity += Problem.getInstance().computeSimilarityCouplingBetweenComponents(component1.getValue(), component2.getValue()) / denominator;
-                }
-            }
-        }
-        intraConnectivity /= decomposition.size();
-        interConnectivity /= (decomposition.size() * (decomposition.size() - 1)) / 2;
-        return intraConnectivity - interConnectivity;
-    }
-
-    public static Double computeCouplingBasedMQ(Map<Long, Set<Long>> decomposition) {
-        Double intraConnectivity = 0d;
-        Double interConnectivity = 0d;
-        for (Map.Entry<Long, Set<Long>> component1 : decomposition.entrySet()) {
-            long[] ids1 = component1.getValue().stream().mapToLong(l -> l).toArray();
-            int denominator = ids1.length == 1 ? 1 : ((ids1.length * (ids1.length - 1)));
-            intraConnectivity += Problem.getInstance().computeCouplingCohesionInComponent(component1.getValue()) / denominator;
+            intraConnectivity += Problem.getInstance().computeCohesionInComponent(component1.getValue()) / denominator;
             for (Map.Entry<Long, Set<Long>> component2 : decomposition.entrySet()) {
                 if (!Objects.equals(component1.getKey(), component2.getKey())) {
                     long[] ids2 = component2.getValue().stream().mapToLong(l -> l).toArray();
@@ -47,7 +28,10 @@ public class ModularizationQualityCalculator {
             }
         }
         intraConnectivity /= decomposition.size();
-        interConnectivity /= (decomposition.size() * (decomposition.size() - 1));
+        interConnectivity /= (decomposition.size() * (decomposition.size() - 1)) / 2;
+        if (Problem.getInstance() instanceof SimilarityProblem) {
+            interConnectivity /= 2;
+        }
         return intraConnectivity - interConnectivity;
     }
 }
