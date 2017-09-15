a1a49bc913f26f703b955fc5d9bdd466d00800e2
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-09-15 11:07:58 +0200

    #24 JavaFX + Spring Boot Application Startup

--- src/main/java/com/buschmais/sarf/SARFRunner.java
+++ src/main/java/com/buschmais/sarf/SARFRunner.java
@@ -26,9 +26,16 @@ import com.buschmais.xo.api.XOManagerFactory;
 import com.buschmais.xo.api.bootstrap.XO;
 import com.buschmais.xo.api.bootstrap.XOUnit;
 import com.buschmais.xo.neo4j.embedded.api.EmbeddedNeo4jXOProvider;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -39,49 +46,56 @@ import java.util.Properties;
 /**
  * Created by steph on 04.05.2017.
  */
-public class SARFRunner {
+public class SARFRunner extends Application{
 
     private static final Logger LOG = LogManager.getLogger(SARFRunner.class);
 
     public static XOManager xoManager;
 
-    public static void main(String[] args) throws URISyntaxException {
+    private ConfigurableApplicationContext springContext;
 
-        Options options = new Options();
-        Option store = new Option("s", "store", true, "Neo4J Graph Store Path");
-        store.setRequired(true);
-        Option config = new Option("c", "conf", true, "Configuration File Path");
-        config.setRequired(false);
-        Option load = new Option("l", "load", true, "Iteration to load");
-        load.setRequired(false);
-        Option bench = new Option("b", "bench", true, "Benchmark File for Genetic Algorithm");
-        bench.setRequired(false);
-        OptionGroup group = new OptionGroup();
-        group.addOption(config);
-        group.addOption(load);
-        group.addOption(bench);
-        group.setRequired(false);
-        options.addOption(store);
-        options.addOptionGroup(group);
-        ClassificationRunner runner = ClassificationRunner.getInstance();
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = null;
-        try {
-            cmd = parser.parse(options, args);
-            URI storeUri = new URI(cmd.getOptionValue("s"));
-            URL configUrl = cmd.hasOption("c") ? new URL(cmd.getOptionValue("c")) : null;
-            Integer iteration = cmd.hasOption("l") ? Integer.valueOf(cmd.getOptionValue("l")) : null;
-            URL benchUrl = cmd.hasOption("b") ? new URL(cmd.getOptionValue("b")) : null;
-            setUpDB(storeUri);
-            runner.run(configUrl, benchUrl, iteration);
-        } catch (ParseException e) {
-            LOG.error(e.getMessage());
-            formatter.printHelp("java -jar sarf.jar", options);
-            System.exit(1);
-        } catch (MalformedURLException e) {
-            LOG.error("Configuration file not found");
-            System.exit(1);
+    private Parent root;
+
+    public static void main(String[] args) throws URISyntaxException {
+        if (args.length == 0) {
+            launch(SARFRunner.class, args);
+        } else {
+            Options options = new Options();
+            Option store = new Option("s", "store", true, "Neo4J Graph Store Path");
+            store.setRequired(true);
+            Option config = new Option("c", "conf", true, "Configuration File Path");
+            config.setRequired(false);
+            Option load = new Option("l", "load", true, "Iteration to load");
+            load.setRequired(false);
+            Option bench = new Option("b", "bench", true, "Benchmark File for Genetic Algorithm");
+            bench.setRequired(false);
+            OptionGroup group = new OptionGroup();
+            group.addOption(config);
+            group.addOption(load);
+            group.addOption(bench);
+            group.setRequired(false);
+            options.addOption(store);
+            options.addOptionGroup(group);
+            ClassificationRunner runner = ClassificationRunner.getInstance();
+            CommandLineParser parser = new DefaultParser();
+            HelpFormatter formatter = new HelpFormatter();
+            CommandLine cmd = null;
+            try {
+                cmd = parser.parse(options, args);
+                URI storeUri = new URI(cmd.getOptionValue("s"));
+                URL configUrl = cmd.hasOption("c") ? new URL(cmd.getOptionValue("c")) : null;
+                Integer iteration = cmd.hasOption("l") ? Integer.valueOf(cmd.getOptionValue("l")) : null;
+                URL benchUrl = cmd.hasOption("b") ? new URL(cmd.getOptionValue("b")) : null;
+                setUpDB(storeUri);
+                runner.run(configUrl, benchUrl, iteration);
+            } catch (ParseException e) {
+                LOG.error(e.getMessage());
+                formatter.printHelp("java -jar sarf.jar", options);
+                System.exit(1);
+            } catch (MalformedURLException e) {
+                LOG.error("Configuration file not found");
+                System.exit(1);
+            }
         }
     }
 
@@ -125,5 +139,19 @@ public class SARFRunner {
         return factory;
     }
 
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(SARFRunner.class);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/root.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        this.root = fxmlLoader.load();
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("SAR-Framework");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
