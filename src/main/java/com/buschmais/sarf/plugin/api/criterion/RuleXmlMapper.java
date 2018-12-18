e5227e5bd037845ef7e0f00ebe6bed833db36397
Stefan Dellmuth <stefan.dellmuth@msg.group>
2018-12-18 17:32:26 +0100

    introduce a-priori knowlegde (package naming rules)

--- src/main/java/com/buschmais/sarf/plugin/api/criterion/RuleXmlMapper.java
+++ src/main/java/com/buschmais/sarf/plugin/api/criterion/RuleXmlMapper.java
@@ -1,47 +1,18 @@
 package com.buschmais.sarf.plugin.api.criterion;
 
-import com.buschmais.sarf.framework.metamodel.ComponentXmlMapper;
-import com.buschmais.sarf.plugin.api.XmlMapper;
-import lombok.*;
-
-import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAttribute;
+
+import com.buschmais.sarf.plugin.api.XmlMapper;
 
 /**
  * @author Stephan Pirnbaum
  */
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
-@AllArgsConstructor
-@EqualsAndHashCode
-public abstract class RuleXmlMapper implements XmlMapper, Comparable<RuleXmlMapper> {
+public abstract class RuleXmlMapper implements XmlMapper {
 
-    @Getter
-    @Setter
-    String shape;
-
-    @Getter
-    @Setter
-    String name;
-
-    @Getter
     @XmlAttribute(name = "weight")
-    double weight;
+    public double weight;
 
-    @Getter
     @XmlAttribute(name = "rule")
-    protected String rule;
+    public String rule;
 
-    @Override
-    public int compareTo(RuleXmlMapper o) {
-        if (!shape.equals(o.getShape())) return shape.compareTo(o.getShape());
-        if (!name.equals(o.getName())) return name.compareTo(o.getName());
-        if (!rule.equals(o.getRule())) return rule.compareTo(o.getRule());
-        return (int) (weight - o.getWeight());
-    }
-
-    @SuppressWarnings("unused")
-    public void afterUnmarshal(Unmarshaller unmarshaller, Object parent) {
-        this.shape = ((ComponentXmlMapper) parent).getShape();
-        this.name = ((ComponentXmlMapper) parent).getName();
-    }
 }
